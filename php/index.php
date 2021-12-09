58090c2e4760f6d641f837fbcdb78c0c529fc1cd
pietruszka123 <56603721+pietruszka123@users.noreply.github.com>
2021-12-09 14:10:38 +0100

    Update index.php

--- php/index.php
+++ php/index.php
@@ -163,7 +163,7 @@
         </div>
     </div>
 
-    <!-- stopka -->
+    <!-- stopkaa -->
     <footer class="relative bottom-0 h-32 dark:text-gray-100 text-pink-300 bg-indigo-800 dark:bg-gray-900 flex justify-center align-bottom">
         <span class="gap-2 absolute bottom-3">
             <a href="">O recyklingu </a>|
