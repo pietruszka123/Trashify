c5b2c27acefdd0e043a5dd488e3e5e634aed4ecb
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2021-12-10 00:05:57 +0100

    kamerka skonczona

---
+++ php/testy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<label class="toggle" for="myToggle">
+    <input class="toggle__input" name="" type="checkbox" id="myToggle">
+    <div class="toggle__fill"></div>
+</label>
+<label class="toggle" for="myToggle">
+            <input class="toggle__input flex justify-center w-full h-5 mb-2 items-center" name="" type="checkbox" id="myToggle">
+            <div class="toggle__fill"></div>
+        </label>
+</body>
+</html>
+<style>
+    
+
+</style>
