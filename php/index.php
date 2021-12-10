6b8aae878bc112ad1ff37cc3cbd51e8cf794cfbd
Gami13 <gamiofficial0@gmail.com>
2021-12-10 11:22:25 +0100

    wyglad login.php jako taki

--- php/index.php
+++ php/index.php
@@ -7,26 +7,25 @@ require_once("header.php");
 
 <!-- container -->
 
-    <!-- skaner -->
-    <div class=" w-72 h-fit dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid border-gray-600">
-        <video id="video" class="fill-current bg-gray-600 mb-2 rounded-xl max-h-60 h-60 border-4 border-gray-400"></video>
-        <label class="toggle" for="startvideo">
-            <input class="toggle__input" name="" type="checkbox" id="startvideo">
-            <div class="toggle__fill"></div>
-        </label>
-        <label class="flex flex-row gap-2 mb-2 justify-center items-center flex-wrap">
+<!-- skaner -->
+<div class=" w-72 h-fit dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid border-gray-600">
+    <video id="video" class="fill-current bg-gray-600 mb-2 rounded-xl max-h-60 h-60 border-4 border-gray-400"></video>
+    <label class="toggle" for="startvideo">
+        <input class="toggle__input" name="" type="checkbox" id="startvideo">
+        <div class="toggle__fill"></div>
+    </label>
+    <label class="flex flex-row gap-2 mb-2 justify-center items-center flex-wrap">
 
-            <select class="bg-gray-500 text-white pl-3 rounded-xl" id="sourceSelect" hidden>
-            </select>
-            <input type="text" id="codeInput" class="bg-gray-500 text-white pl-3 rounded-xl">
-            <button id="submit" class="p-2 text-white  justify-center items-center flex rounded"><i class="far fa-paper-plane"></i></button>
-        </label>
+        <select class="bg-gray-500 text-white pl-3 rounded-xl" id="sourceSelect" hidden></select>
+        <input type="text" id="codeInput" class="bg-gray-500 text-white pl-3 rounded-xl">
+        <button id="submit" class="p-2 text-white  justify-center items-center flex rounded"><i class="far fa-paper-plane"></i></button>
+    </label>
 
-    </div>
-    <!-- Historia -->
-    <div class="dark:bg-gray-700  border-4 border-solid dark:border-white rounded-2xl border-gray-600 w-72 h-80">
+</div>
+<!-- Historia -->
+<div class="dark:bg-gray-700  border-4 border-solid dark:border-white rounded-2xl border-gray-600 w-72 h-80">
 
-    </div>
+</div>
 
 <script src="./js/scanner.js"></script>
 <!-- stopka -->
--- php/login.php
+++ php/login.php
@@ -5,7 +5,7 @@ require_once("header.php");
 
 
 <!-- container -->
-<?php 
+<?php
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -15,58 +15,78 @@ $mysqli = new mysqli(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 
 
-if (isset($_COOKIE['token']) &&  $_COOKIE['token'] != "notSet" && isTokenValid($_COOKIE['token']) && $_SERVER['REQUEST_URI'] == 'login.php') { //If the token is valid, then sends you to the index page (if already not somewhere else than login page)
+if (isset($_COOKIE['token']) &&  $_COOKIE['token'] != "notSet" && isTokenValid($_COOKIE['token']) && $_SERVER['REQUEST_URI'] == 'login.php')
+{ //If the token is valid, then sends you to the index page (if already not somewhere else than login page)
     //header("Location: index.php");
-} else if (isset($_COOKIE['token']) &&  $_COOKIE['token'] != "notSet" && isTokenValid($_COOKIE['token'])) {
-    ;
-} else if ($_SERVER['REQUEST_URI'] != 'login.php') { //If you don't have a cookie token then sets it to "notSet" and sends you to the login page if you're not there
-   // setcookie("token", "notSet", 0, "/");
-   // header("Location: login.php");
-} else { //If you don't have a cookie token then sets it to "notSet"
+}
+else if (isset($_COOKIE['token']) &&  $_COOKIE['token'] != "notSet" && isTokenValid($_COOKIE['token']))
+{;
+}
+else if ($_SERVER['REQUEST_URI'] != 'login.php')
+{ //If you don't have a cookie token then sets it to "notSet" and sends you to the login page if you're not there
+    // setcookie("token", "notSet", 0, "/");
+    // header("Location: login.php");
+}
+else
+{ //If you don't have a cookie token then sets it to "notSet"
     setcookie("token", "notSet", 0, "/");
 }
 
 
 
 //Set cookie value if available
-if (isset($_SESSION['token']) && isset($_SESSION['rememberMe'])) {
-    setcookie("token", $_SESSION['token'], time() + (3600 * 24 * 31 *12), "/");
-} else if (isset($_SESSION['token']) && !isset($_SESSION['rememberMe'])) {
-    setcookie("token", $_SESSION['token'], 0 , "/");
+if (isset($_SESSION['token']) && isset($_SESSION['rememberMe']))
+{
+    setcookie("token", $_SESSION['token'], time() + (3600 * 24 * 31 * 12), "/");
+}
+else if (isset($_SESSION['token']) && !isset($_SESSION['rememberMe']))
+{
+    setcookie("token", $_SESSION['token'], 0, "/");
 }
 
 //Dump these values
 $_SESSION['token'] = NULL;
 $_SESSION['rememberMe'] = NULL;
 // Logs in the user
-function userLogin() {
-if($_SERVER['REQUEST_METHOD'] == 'POST') {
-global $mysqli;
+function userLogin()
+{
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        global $mysqli;
 
-if (isset($_POST['rememberMe'])) {
-$_SESSION['rememberMe'] = 1;
-}
+        if (isset($_POST['rememberMe']))
+        {
+            $_SESSION['rememberMe'] = 1;
+        }
 
-if (emailCorrect($_POST['email']) && passwordCorrect($_POST['password'])) { //Checks if the input is correct
-$sql = "SELECT `users`.`id`, `users`.`password` FROM `users` WHERE `users`.`email` = ?";
+        if (emailCorrect($_POST['email']) && passwordCorrect($_POST['password']))
+        { //Checks if the input is correct
+            $sql = "SELECT `users`.`id`, `users`.`password` FROM `users` WHERE `users`.`email` = ?";
 
-if ($stmt = $mysqli->prepare($sql)) { //Searches for the user id and password
-$stmt->bind_param("s", $_POST['email']);
-if($stmt->execute()) {
-$result = $stmt->get_result();
-if($result->num_rows != 0) {
-$row = $result->fetch_assoc();
-    
-if ($row['password'] == $_POST['password']) { //Checks if the password given by the user is correct
-$userId = $row['id'];
-$sql = "SELECT `users`.`token` FROM `users` WHERE `users`.`id` = $userId";
+            if ($stmt = $mysqli->prepare($sql))
+            { //Searches for the user id and password
+                $stmt->bind_param("s", $_POST['email']);
+                if ($stmt->execute())
+                {
+                    $result = $stmt->get_result();
+                    if ($result->num_rows != 0)
+                    {
+                        $row = $result->fetch_assoc();
 
-$result = $mysqli->query($sql);
-$row = $result->fetch_assoc();
+                        if ($row['password'] == $_POST['password'])
+                        { //Checks if the password given by the user is correct
+                            $userId = $row['id'];
+                            $sql = "SELECT `users`.`token` FROM `users` WHERE `users`.`id` = $userId";
 
-                            if (isset($row['token']) && $row['token'] != NULL) { //Checks if the the token is still valid and logs in the user
+                            $result = $mysqli->query($sql);
+                            $row = $result->fetch_assoc();
+
+                            if (isset($row['token']) && $row['token'] != NULL)
+                            { //Checks if the the token is still valid and logs in the user
                                 $_SESSION['token'] = $row['token'];
-                            } else {
+                            }
+                            else
+                            {
                                 $token = generateToken();
                                 $_SESSION['token'] = $token;
 
@@ -75,37 +95,53 @@ $row = $result->fetch_assoc();
                             }
 
                             header("Location: index.php");
-                        } else {
+                        }
+                        else
+                        {
                             echo 'Wrong email or password';
                         }
-                    } else {
+                    }
+                    else
+                    {
                         echo 'ERROR: SF3';
                     }
-                } else {
+                }
+                else
+                {
                     echo 'ERROR: SF2';
                 }
-            } else {
+            }
+            else
+            {
                 echo 'ERROR: SF1';
             }
-        } else {
+        }
+        else
+        {
             echo 'Wrong email or password';
         }
     }
 }
 //Checks if the user token is still valid
-function isTokenValid($cookie) {
+function isTokenValid($cookie)
+{
     global $mysqli;
 
     $sql = "SELECT id, token FROM `users` WHERE token = ?";
 
-    if($stmt = $mysqli->prepare($sql)) {
+    if ($stmt = $mysqli->prepare($sql))
+    {
         $stmt->bind_param("s", $cookie);
-        if($stmt->execute()) {
+        if ($stmt->execute())
+        {
             $result = $stmt->get_result();
-            
-            if ($result->num_rows == 1) {
+
+            if ($result->num_rows == 1)
+            {
                 return true;
-            } else {
+            }
+            else
+            {
                 return false;
             }
         }
@@ -113,27 +149,36 @@ function isTokenValid($cookie) {
 }
 
 // Looks if the email given by the user is correct
-function emailCorrect($email) {
-    if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
+function emailCorrect($email)
+{
+    if (filter_var($email, FILTER_VALIDATE_EMAIL))
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 // Looks if the password given by the user is correct
-function passwordCorrect($password) {
+function passwordCorrect($password)
+{
     $correctness = preg_match('/^[a-zA-Z0-9]{3,64}$/', $password);
 
-    if($correctness == 1) {
+    if ($correctness == 1)
+    {
         return true;
-    } else {
+    }
+    else
+    {
         return false;
     }
 }
 
 // Generates a random token
-function generateToken() {
+function generateToken()
+{
     $bytes = random_bytes(64);
     return bin2hex($bytes);
 }
@@ -142,31 +187,26 @@ userLogin();
 ?>
 
 
-<div class="login-grid center">
-  <div class="login-box">
-    <h1>Log in</h1>
+<div class="w-72 h-fit dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid border-gray-600 justify-center flex flex-wrap text-center p-3">
+   
     <form method="POST">
+    <h1 class="text-3xl pt-1">Zaloguj się</h1>
+        <input type="email" id="email" name="email" class="w-10/12 p-2 m-6 bg-gray-500 text-white rounded-xl" placeholder="Email"><br>
+        <input type="password" id="password" name="password" class="w-10/12 p-2 m-6 bg-gray-500 text-white rounded-xl" placeholder="Password" pattern="^[a-zA-Z0-9]{3,64}$"><br>
 
-      <div class="loginInputBox">
-        <input type="email" id="email" name="email" class="loginInput" placeholder="Email"><br>
-        <input type="password" id="password" name="password" class="loginInput" placeholder="Password" pattern="^[a-zA-Z0-9]{3,64}$"><br>
-      </div>
 
-      <div class="loginRememberAndSubmit">
-        <div class="loginRememberMeBox">
-          <label for="loginRemember" class="loginRememberMe">Remember me:
+
+        <label for="loginRemember" class="loginRememberMe">Zapamiętaj mnie:</label>
             <input type="checkbox" id="rememberMe" name="rememberMe" class="loginRememberMeCheckBox">
-          </label>
-        </div>
-        <input type="submit" id="submit" name="submit" class="loginSubmit" value="Log in">
-      </div>
+        
+
+        <input type="submit" id="submit" name="submit" class="w-1/2" value="Zaloguj">
+
 
     </form>
-  </div>
 </div>
 
-
-<?php 
+<?php
 include_once('footer.php');
 ?>
 
