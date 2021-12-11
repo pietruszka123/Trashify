f8e7338556bf98cc8e4c144003c769ea2efd6401
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2021-12-11 13:54:24 +0100

    mapa

--- php/mapa.php
+++ php/mapa.php
@@ -5,14 +5,13 @@ require_once("header.php");
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.9.0/css/ol.css">
 
 <!-- container -->
-<<<<<<< Updated upstream
+
 
 <div id="map" class="map w-full h-80">
-=======
 <h1 class="w-full text-3xl font-bold text-center text-pink-500">Mapa</h1>
 <h2 class="w-full text-xl font-bold text-center text-blue-500">(Lokalizacje śmietników)</h2>
 <div id="map" class="map w-80 h-80 dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid border-gray-600 p-3">
->>>>>>> Stashed changes
+
 
 </div>
 <input type="checkbox" id="add">
