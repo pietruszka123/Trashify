00256a18cc92d380b85a8e040b2a71ff46a0023c
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2021-12-12 11:38:55 +0100

    mapa skończona

--- php/mapa.php
+++ php/mapa.php
@@ -16,28 +16,38 @@ require_once("header.php");
 <!-- container -->
 
 
-<div class="map w-72 h-0 sm:w-1/2 sm:pb-1/2 h-fit dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid flex flex-wrap border-gray-600 p-3">
+<div class="map w-72 h-0 sm:w-1/2 sm:pb-1/2 h-fit gap-4 dark:bg-gray-700 dark:border-white border-4 rounded-2xl border-solid flex flex-wrap border-gray-500 p-3">
     <h1 class="w-full text-3xl font-bold text-center text-pink-500">Mapa</h1>
     <h2 class="w-full text-xl font-bold text-center text-blue-500">(Lokalizacje śmietników)</h2>
     <div id="map" class="map w-full h-60 mapa">
         <p id="Loading" class="absolute -z-50">ŁADOWANIE...</p>
 
     </div>
-    <input type="checkbox" id="add">
-    <input type="text" class="w-full h-10 p-3 m-2 bg-gray-500 text-white focus:outline-none rounded-xl text-xl" placeholder="longitude" id="longitude" pattern="[0-9.]+">
-    <input type=" text" id="latitude" pattern="[0-9.]+" placeholder="latitude" class="w-full h-10 p-3 m-2 bg-gray-500 text-white focus:outline-none rounded-xl text-xl">
+    <label for="" class="flex w-full gap-4 text-center ite">
+        
+        <p class="font-bold ml-2 bg-gray-500 px-2 rounded-md">dodaj kosz</p>
+        <input type="checkbox" id="add" class="h-6 w-6 font-bold bg-gray-500"></label>
+        <label class="flex w-full gap-4 items-center">
+            <p class="font-bold bg-gray-500 p-1 rounded-md ml-2 px-3">Rodzaj kosza</p>
+            <select id="type" class="bg-gray-500 font-bold p-1 rounded-md">
+                <option value="mieszane" class="bg-black sm:w-15">mieszane</option>
+                <option value="papier" class="bg-blue-800">papier</option>
+                <option value="szklo" class="bg-black text-white">szkło</option>
+                <option value="plastik" class="bg-yellow-300 text-black">metale i plastik</option>
+                <option value="bio" class="bg-green-800">Biodegradowalne</option>
+                <option value="baterie">baterie</option>
+                <option value="leki " class="bg-red-700 focus:outline-none">
+                    <p class="text-bold">Lerstwa +</p>
+                </option>
+            </select>
+        </label>
+    </label>
+    <input type="text" class="w-full h-10 p-3 m-2 bg-gray-500 text-white focus:outline-none rounded-md text-xl" placeholder="longitude" id="longitude" pattern="[0-9.]+">
+    <input type=" text" id="latitude" pattern="[0-9.]+" placeholder="latitude" class="w-full h-10 p-3 m-2 bg-gray-500 text-white focus:outline-none rounded-md text-xl">
     <!-- obrazek<input type="file" id=""> -->
-    <select id="type" class="bg-gray-400 p-1 rounded-md">
-        <option value="mieszane" class="bg-black">mieszane</option>
-        <option value="papier" class="bg-blue-800">papier</option>
-        <option value="szklo" class="bg-green-600 text-black">szkło</option>
-        <option value="plastik" class="bg-yellow-300 text-black">metale i plastik</option>
-        <option value="bio" class="bg-yellow-900">Biodegradowalne</option>
-        <option value="baterie">baterie</option>
-        <option value="leki">leki</option>
-    </select>
+
     <!-- dodatkowe informacje<textarea id="" cols="30" rows="10" required></textarea> -->
-    <input type="button" value="Zapisz" id="save" class="w-full h-10 p-3 m-2 bg-gray-500 text-white focus:outline-none rounded-xl pb-4 text-xl">
+    <input type="button" value="Zapisz" id="save" class="w-fit mx-auto py-1/2 h-fit p-3 m-2 bg-gray-500 text-pink-300 font-bold focus:outline-none rounded-xl pb-4 text-xl">
     <script src="./js/mapa.js"></script>
     <!-- stopka -->
 </div>
