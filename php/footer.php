93bb2a1f75f2ce24759258df78b39f18d952b085
SeKaFi69 <92319044+SeKaFi69@users.noreply.github.com>
2021-12-13 13:20:48 +0100

    artykuły

--- php/footer.php
+++ php/footer.php
@@ -1,10 +1,11 @@
 </div>
-<footer class="w-screen relative h-20 dark:text-gray-100 text-pink-300 bg-indigo-800 dark:bg-gray-900 items-self-end flex justify-center ">
+
+<footer class="w-screen relative h-20 dark:text-gray-100  text-pink-300 bg-indigo-800 dark:bg-gray-900 items-self-end flex justify-center">
     <span class="items-end flex mb-4 gap-1">
-    <a href="./index.php">Strona główna </a><p>|</p>
-        <a href="./mapa.php">Mapa </a><p>|</p>
-        <a href="./informacje.php">Artykuły </a><p>|</p>
-        <a href="./tworcy.php">Autorzy </a><p>|</p>
+    <a href="./index.php" class="underline">Strona główna </a><p>|</p>
+        <a href="./mapa.php" class="underline">Mapa </a><p>|</p>
+        <a href="./informacje.php" class="underline">Artykuły </a><p>|</p>
+        <a href="./tworcy.php" class="underline">Autorzy </a>
     </span>
 
 </footer>
--- php/informacje.php
+++ php/informacje.php
@@ -1,11 +1,72 @@
 <?php
 require_once("header.php");
 ?>
+<!-- container z hedera -->
 
 
+<!-- <table class="max-w-60 bg-gray-700">
 
-<!-- container -->
+<thead>
+  <tr>
+    <th colspan="2" class="text-center text-2xl p-2 mb-3">Artykuły o recyklingu ,które warto przeczytać.</th>
+  </tr>
+</thead>
+<tbody>
+  <tr class="border border-white w-full flex flex-wrap p-2 mb-2">
+    <td class="w-1/5 text-xl"><a href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds" target="_blank" rel="noopener noreferrer" class=" underline">Czym jest recykling?</a></td>
+    <td class="overflow-x-hidden"><a class="w-3/5" href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds">https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds</a></td>
+  </tr>
 
+  <tr class="border border-white w-full flex flex-wrap p-2 mb-2">
+    <td class="w-1/5 text-xl"><a href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds" target="_blank" rel="noopener noreferrer" class=" underline">Czym jest recykling?</a></td>
+    <td class="overflow-x-hidden"><a class="w-3/5" href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds">https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds</a></td>
+  </tr>
+
+  <tr class="border border-white w-full flex flex-wrap p-2 mb-2">
+    <td class="w-1/5 text-xl"><a href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds" target="_blank" rel="noopener noreferrer" class=" underline">Czym jest recykling?</a></td>
+    <td class="overflow-x-hidden"><a class="w-3/5" href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds">https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds</a></td>
+  </tr>
+</tbody>
+</table> -->
+<div class="p-2 border-white border-2 w-5/6 max-w-80 h-fit bg-gray-700">
+<table class="w-full ">
+    <tr>
+        <th class="font-bold text-center text-lg  pb-4">Artykuły o recyklingu ,które warto przeczytać.</th>
+    </tr>
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://velvet.pl/recykling-co-to-jest/?&utm_source=GoogleAds&utm_medium=CPC&utm_content=DSA&utm_term=DSA&utm_campaign=PL_Velvet_DSA_Dla_Natury&gclid=CjwKCAiA-9uNBhBTEiwAN3IlNDYPi0kTjSF4cl0JaLQBrYQXjVejxKEyQxrWF_l1R043PDLarm0jnhoCHj4QAvD_BwE&gclsrc=aw.ds" target="_blank" rel="noopener noreferrer">Czym jest recykling?</a></td>
+    </tr>
+
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://higieniczny.pl/recykling-i-plusy-tego-rozwiazania.html" target="_blank" rel="noopener noreferrer">Plusy segregowania śmieci</a></td>
+    </tr>
+
+     <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://www.opakowanianawynos.pl/blog/news/recykling-czyli-jak-zaoszczedzic-kilka-groszy" target="_blank" rel="noopener noreferrer">Jak zaoszczędzić na recyklingu?</a></td>
+    </tr>
+
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://naszesmieci.mos.gov.pl/sens-recyklingu" target="_blank" rel="noopener noreferrer">Czy recykling ma sens?</a></td>
+    </tr>
+
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="http://ekoswiat.eu/recykling-gumy/" target="_blank" rel="noopener noreferrer">Recykling gumy i opon</a></td>
+    </tr>
+
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://www.wiadomoscikosmetyczne.pl/artykuly/od-odpadu-do-odpadu-jak-wyglada-recykling-tworzyw-,64269" target="_blank" rel="noopener noreferrer">Jak wygląda proces recyklingu?</a></td>
+    </tr>
+    
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://www.krn.pl/artykul/recykling-materialow-budowlanych-na-czym-polega-i-jak-wyglada-w-polsce,3526" target="_blank" rel="noopener noreferrer">Recykling materiałów budowlanych</a></td>
+    </tr>
+
+    <tr class="border-white border">
+        <td class="p-3 text-pink-300"><a class="underline font-semibold" href="https://gethome.pl/blog/segregacja-odpadow-co-trzeba-wiedziec/" target="_blank" rel="noopener noreferrer">Segregacja różnych odpadów</a></td>
+    </tr>
+    
+</table>
+</div>
 
 <!-- stopka -->
 <?php include_once('footer.php');
